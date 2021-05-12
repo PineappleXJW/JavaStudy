103998a5cb0493f45c732473d49de61f2d740e8a
PineappleXJW <302409013@qq.com>
2021-05-12 13:22:30 +0800

    整理了一下前段时间所学过的笔记，开始学习匿名内部类

---
+++ src/com/hunau/AnonymousInnerClass/Test01.java
@@ -0,0 +1,23 @@
+package com.hunau.AnonymousInnerClass;
+
+/**
+ * @Systemname:Java学习练习
+ * @Author:谢俊伟
+ * @Description:
+ * @Data:Created in 2021/5/12 13:17
+ * @Unitname:湖南农业大学信科学院物联网工程系
+ * @Copyright:谢俊伟 201841882121
+ */
+
+/**
+ * 匿名内部类：
+ *     1、什么是内部类？
+ *         内部类：在类的内部又定义了一个新的类，被称为内部类
+ *     2、内部类的分类：
+ *         静态内部类：类似于静态变量
+ *         实力内部类：类似于实例变量
+ *         局部内部类：类似于局部变量
+ *
+ */
+public class Test01 {
+}
--- src/com/hunau/InterfaceTest/Readme.txt
+++ src/com/hunau/InterfaceTest/Readme.txt
@@ -1,14 +1,3 @@
-package com.hunau.InterfaceTest;
-
-/**
- * @Systemname:Java学习练习
- * @Author:谢俊伟
- * @Description:
- * @Data:Created in 2021/3/15 21:20
- * @Unitname:湖南农业大学信科学院物联网工程系
- * @Copyright:谢俊伟 201841882121
- */
-
 /**
  * 抽象类：
  * 	1、什么是抽象类？
@@ -85,5 +74,3 @@ package com.hunau.InterfaceTest;
  * 	接口可以将调用者和实现者解耦合。
  *
  */
-public class Readme {
-}
